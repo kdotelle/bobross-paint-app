d7a65373cc7aca98076223ca35c2199f0d4a6416
Kristen <lloyd.kristen91@gmail.com>
2021-11-17 16:22:18 -0500

    installed canvasdraw, added component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import Canvas from "./Canvas.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Canvas.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+import CanvasDraw from "react-canvas-draw";
+
+import "./styles/CanvasStyles.css";
+
+function Canvas() {
+  const [canvas, setBrush] = useState("#FCA5A5");
+  const [brush, setThick] = useState(50);
+  return (
+    <div>
+      <h1>Canvas Component</h1>
+      <CanvasDraw
+        className="canvas"
+        hideGrid
+        canvasWidth="300"
+        canvasHeight="400"
+        lazyRadius="0"
+        enablePanAndZoom
+      />
+    </div>
+  );
+}
+
+export default Canvas;
