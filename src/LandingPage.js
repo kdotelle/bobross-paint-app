ce7d7353dcdc8a82e4b0e6d15b366f9246e0682d
Kristen <lloyd.kristen91@gmail.com>
2021-11-22 11:09:59 -0500

    footer added

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,39 +1,66 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { Button } from "reactstrap";
+import SvgIcon from "@mui/material/SvgIcon";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import "./styles/LandingPageStyles.css";
 
 function LandingPage() {
   return (
-    <div className="container-fluid">
-      <div className="p-5 mb-4 bg-light rounded-3">
-        <h1 className="header fw-bold">Paint Like Bob Ross</h1>
-        <div className="row">
-          <div className="col-sm-8">
-            <h3 className="display-5 fw-bold">
-              We don't make mistakes, just happy little accidents.
-            </h3>
-            <p>
-              Discover the artist in you and embrace your creative side.
-              Transform the canvas with creativity and color. Draw free form or
-              fill the canvas with one click.
-            </p>
+    <div>
+      <div className="container-fluid">
+        <div className="p-5 mb-4 bg-light rounded-3">
+          <h1 className="header fw-bold">Paint Like Bob Ross</h1>
+          <div className="row">
+            <div className="col-sm-8">
+              <h3 className="display-5 fw-bold">
+                We don't make mistakes, just happy little accidents.
+              </h3>
+              <p>
+                Discover the artist in you and embrace your creative side.
+                Transform the canvas with creativity and color. Draw free form
+                or fill the canvas with one click.
+              </p>
+            </div>
+            <div className="col-sm-4">
+              <Link to="/draw">
+                <Button block color="primary" size="lg">
+                  Try it!
+                </Button>
+              </Link>
+            </div>
           </div>
-          <div className="col-sm-4">
-            <Link to="/draw">
-              <Button block color="primary" size="lg">
-                Try it!
-              </Button>
-            </Link>
+        </div>
+        <div className="row align-items-md-stretch">
+          <div className="col-md-6">
+            <div className="p-5 text-white bg-dark border rounded-3">
+              <h3 className="display-5 fw-bold">About Me</h3>
+              <p>
+                I'm a driven Junior MERN Stack Developer based out of Northern
+                VA. I use web development to bridge my passions of creativity
+                and problem solving.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col">
-          <h3>About Me</h3>
+      <footer className="footer mt-auto py-3 bg-light fixed-bottom">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-4 text-center">
+              <h5>Social</h5>
+              <LinkedInIcon color="primary" fontSize="large" />
+              <GitHubIcon fontSize="large" />
+              <TwitterIcon color="primary" frontSize="large" />
+            </div>
+            <div className="col-4 text-center">
+              <p>Copyright © Kristen Lloyd 2021</p>
+            </div>
+          </div>
         </div>
-      </div>
+      </footer>
     </div>
   );
 }
